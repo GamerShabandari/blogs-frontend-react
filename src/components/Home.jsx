1f44e447caa562cd44d86d0e51c70bbdf7499aa7
GamerSh <93838417+GamerShabandari@users.noreply.github.com>
2022-05-12 21:43:20 +0200

    fixat lite css text och animationer

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,7 @@
 import axios from "axios";
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
+
 
 export function Home() {
 
@@ -40,7 +42,7 @@ export function Home() {
 
     useEffect(() => {
         let myUserIdSerialized = localStorage.getItem("myUserId");
-      
+
         if (myUserIdSerialized) {
             let myUserIdDeSerialized = JSON.parse(localStorage.getItem("myUserId"));
             setLoggedIn(true);
@@ -231,27 +233,42 @@ export function Home() {
     //////////////////////////////////////////////////////////////////////////////////////////////////////
 
     let usersBlogsListHtml = usersBlogs.map((blog, i) => {
-        return (<div className="blogCard" key={i}>
+        return (<motion.div className="blogCard" key={i}
+            initial={{ opacity: 0, translateY: -30 }}
+            animate={{ opacity: 1, translateY: 0 }}
+            transition={{ ease: "easeInOut", duration: 0.2, delay: i * 0.4 }}
+
+        >
             <h3>{blog.title}</h3>
             <div>{blog.text}</div>
             <h6>{blog.created}</h6>
-            <button onClick={() => { deleteBlog(blog.id) }}>X</button>
-            <button onClick={() => { editBlog(i) }}>edit</button>
-        </div>)
+            <button className="deleteBtn" onClick={() => { deleteBlog(blog.id) }}>x</button>
+            <button className="editBtn" onClick={() => { editBlog(i) }}>edit</button>
+        </motion.div>)
     })
 
     let usersBlogsListForGuestsHtml = usersBlogs.map((blog, i) => {
-        return (<div className="blogCard" key={i}>
+        return (<motion.div className="blogCard"
+            key={i}
+            initial={{ opacity: 0, translateY: -20 }}
+            animate={{ opacity: 1, translateY: 0 }}
+            transition={{ ease: "easeInOut", duration: 0.3, delay: i * 0.3 }}
+        >
             <h3>{blog.title}</h3>
             <div>{blog.text}</div>
             <h6>{blog.created}</h6>
-        </div>)
+        </motion.div>)
     })
 
     let allUsersList = allUsers.map((user, i) => {
         return (<div className="userContainer" key={i}>
 
-            <div onClick={() => { fetchUsersBlogs(user.id) }}>{user.name.charAt(0).toUpperCase() + user.name.slice(1)}</div>
+            <motion.div onClick={() => { fetchUsersBlogs(user.id) }}
+                initial={{ opacity: 0, translateY: -20 }}
+                animate={{ opacity: 1, translateY: 0 }}
+                transition={{ ease: "easeInOut", duration: 0.4, delay: i * 0.4 }}>
+                {user.name.charAt(0).toUpperCase() + user.name.slice(1)}
+            </motion.div>
 
         </div>)
     })
@@ -264,16 +281,16 @@ export function Home() {
     return (<>
         <header>
             <h1>BLOGGUS MAXIMUS</h1>
-            {loggedIn && <><h2>Välkommen {yourName}</h2><button onClick={() => { setLoggedIn(false); setUsersBlogs([]); localStorage.removeItem("myUserId"); localStorage.removeItem("yourName") }}>log out</button></>}
+            {loggedIn && <><h2>Välkommen {yourName}</h2><button className="Btn" onClick={() => { setLoggedIn(false); setUsersBlogs([]); localStorage.removeItem("myUserId"); localStorage.removeItem("yourName") }}>log out</button></>}
         </header>
         <main>
             {blogsUpdated && <div><h1>Bloggar Uppdaterade...</h1></div>}
 
             {showEditBookingForm && <div className="editFormContainer">
                 <input type="text" placeholder="title" value={editBlogTitle} onChange={handleEditBlogTitle} />
-                <textarea cols="30" rows="10" placeholder="text" value={editBlogText} onChange={handleEditBlogText}></textarea>
-                <button onClick={saveEditedBlog}>save</button>
-                <button onClick={() => { setShowEditBookingForm(false); setEditBlogTitle(""); setEditBlogText("") }}>cancel</button>
+                <textarea cols="30" rows="40" placeholder="text" value={editBlogText} onChange={handleEditBlogText}></textarea>
+                <button className="Btn"  onClick={saveEditedBlog}>save</button>
+                <button className="Btn"  onClick={() => { setShowEditBookingForm(false); setEditBlogTitle(""); setEditBlogText("") }}>cancel</button>
             </div>}
 
             {!loggedIn && <div>
@@ -281,20 +298,20 @@ export function Home() {
                     <h4>First time here? Create an account!</h4>
                     <input type="text" placeholder="username (atleast 6 characters)" value={createdUsername} onChange={handleCreatedNameInput} />
                     <input type="password" placeholder="password (atleast 6 characters)" value={createdPassword} onChange={handleCreatedPasswordInput} />
-                    <button type="button" onClick={createUser}>create new user</button>
+                    <button type="button" className="Btn" onClick={createUser}>create new user</button>
                 </form>
 
                 <form>
                     <h4>Allready a member, login below</h4>
                     <input type="text" placeholder="username" value={username} onChange={handleNameInput} />
                     <input type="password" placeholder="password" value={password} onChange={handlePasswordInput} />
-                    <button type="button" onClick={login}>login</button>
+                    <button className="Btn" type="button" onClick={login}>login</button>
                 </form>
                 {showError && <div>Fel uppgifter! försök igen</div>}
 
                 <main>
                     <aside>
-                        <div>{allUsersList}</div>
+                        <div className="usersListContainer">{allUsersList}</div>
                         <div>{usersBlogsListForGuestsHtml}</div>
                     </aside>
                 </main>
@@ -308,7 +325,7 @@ export function Home() {
                     <form className="createBlogForm">
                         <input type="text" placeholder="title" value={newBlogTitle} onChange={handleNewBlogTitle} />
                         <textarea cols="30" rows="10" value={newBlogText} onChange={handleNewBlogText}></textarea>
-                        <button type="button" onClick={saveNewBlog}>spara</button>
+                        <button className="Btn" type="button" onClick={saveNewBlog}>spara</button>
                     </form>
                 </div>
                 <div>
